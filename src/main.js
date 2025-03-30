a39ea4d2f073918245ba3a6b808cfde5ac49c2e1
FunWebApp <weba45594@gmail.com>
2025-03-31 01:02:55 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import './style.css'
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.130.1/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.130.1/examples/jsm/controls/OrbitControls.js';
+
 
 
 // scene
